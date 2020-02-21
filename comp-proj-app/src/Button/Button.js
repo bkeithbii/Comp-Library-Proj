8597e38aea12d2d1b4c284f7e9f0ec9e38f62ac4
Brian Brown II <bkeithbii@gmail.com>
2020-02-21 16:04:04 -0500

    Primary through Default complete

--- comp-proj-app/src/Button/Button.js
+++ comp-proj-app/src/Button/Button.js
@@ -8,7 +8,7 @@ const Button = props => {
 
   // Create an array of all of the story/component types you want to be
   // included in your component library
-  let types = ["primary", "danger", "success", "warning"];
+  let types = ["primary", "danger", "success", "warning", "default"];
 
   // Add a conditional statement that checks for the type and updates the
   // classList variable based on their existence.
@@ -35,6 +35,82 @@ const Button = props => {
   }
   if (props.primaryLargeBorder) {
     classList += ` primary-large-border`;
+  }
+
+  if (props.dangerHover) {
+    classList += ` danger-hover`;
+  }
+  if (props.dangerBorder) {
+    classList += ` danger-border`;
+  }
+  if (props.dangerFade) {
+    classList += ` danger-fade`;
+  }
+  if (props.dangerLarge) {
+    classList += ` danger-large`;
+  }
+  if (props.dangerLargeFade) {
+    classList += ` danger-large-fade`;
+  }
+  if (props.dangerLargeBorder) {
+    classList += ` danger-large-border`;
+  }
+
+  if (props.successHover) {
+    classList += ` success-hover`;
+  }
+  if (props.successBorder) {
+    classList += ` success-border`;
+  }
+  if (props.successFade) {
+    classList += ` success-fade`;
+  }
+  if (props.successLarge) {
+    classList += ` success-large`;
+  }
+  if (props.successLargeFade) {
+    classList += ` success-large-fade`;
+  }
+  if (props.successLargeBorder) {
+    classList += ` success-large-border`;
+  }
+
+  if (props.warningHover) {
+    classList += ` warning-hover`;
+  }
+  if (props.warningBorder) {
+    classList += ` warning-border`;
+  }
+  if (props.warningFade) {
+    classList += ` warning-fade`;
+  }
+  if (props.warningLarge) {
+    classList += ` warning-large`;
+  }
+  if (props.warningLargeFade) {
+    classList += ` warning-large-fade`;
+  }
+  if (props.warningLargeBorder) {
+    classList += ` warning-large-border`;
+  }
+
+  if (props.defaultHover) {
+    classList += ` default-hover`;
+  }
+  if (props.defaultBorder) {
+    classList += ` default-border`;
+  }
+  if (props.defaultFade) {
+    classList += ` default-fade`;
+  }
+  if (props.defaultLarge) {
+    classList += ` default-large`;
+  }
+  if (props.defaultLargeFade) {
+    classList += ` default-large-fade`;
+  }
+  if (props.defaultLargeBorder) {
+    classList += ` default-large-border`;
   }
 
   // Give the button's class a value of classList
--- comp-proj-app/src/Button/Button.stories.js
+++ comp-proj-app/src/Button/Button.stories.js
@@ -25,5 +25,79 @@ storiesOf("Button", module)
   ))
 
   .add("Danger", () => <Button label="Danger" type="danger" />)
+  .add("Danger Hover", () => (
+    <Button label="Danger" type="danger" dangerHover />
+  ))
+  .add("Danger Border", () => (
+    <Button label="Danger" type="danger" dangerBorder />
+  ))
+  .add("Danger Fade", () => <Button label="Danger" type="danger" dangerFade />)
+  .add("Danger Large", () => (
+    <Button label="Danger Large" type="danger" dangerLarge />
+  ))
+  .add("Danger Large-Fade", () => (
+    <Button label="Danger Large" type="danger" dangerLargeFade />
+  ))
+  .add("Danger Large-Border", () => (
+    <Button label="Danger Large" type="danger" dangerLargeBorder />
+  ))
+
   .add("Success", () => <Button label="Success" type="success" />)
-  .add("Warning", () => <Button label="Warning" type="warning" />);
+  .add("Success Hover", () => (
+    <Button label="Success" type="success" successHover />
+  ))
+  .add("Success Border", () => (
+    <Button label="Success" type="success" successBorder />
+  ))
+  .add("Success Fade", () => (
+    <Button label="Success" type="success" successFade />
+  ))
+  .add("Success Large", () => (
+    <Button label="Success Large" type="success" successLarge />
+  ))
+  .add("Success Large-Fade", () => (
+    <Button label="Success Large" type="success" successLargeFade />
+  ))
+  .add("Success Large-Border", () => (
+    <Button label="Success Large" type="success" successLargeBorder />
+  ))
+
+  .add("Warning", () => <Button label="Warning" type="warning" />)
+  .add("Warning Hover", () => (
+    <Button label="Warning" type="warning" warningHover />
+  ))
+  .add("Warning Border", () => (
+    <Button label="Warning" type="warning" warningBorder />
+  ))
+  .add("Warning Fade", () => (
+    <Button label="Warning" type="warning" warningFade />
+  ))
+  .add("Warning Large", () => (
+    <Button label="Warning Large" type="warning" warningLarge />
+  ))
+  .add("Warning Large-Fade", () => (
+    <Button label="Warning Large" type="warning" warningLargeFade />
+  ))
+  .add("Warning Large-Border", () => (
+    <Button label="Warning Large" type="warning" warningLargeBorder />
+  ))
+
+  .add("Default", () => <Button label="Default" type="default" />)
+  .add("Default Hover", () => (
+    <Button label="Default" type="default" defaultHover />
+  ))
+  .add("Default Border", () => (
+    <Button label="Default" type="default" defaultBorder />
+  ))
+  .add("Default Fade", () => (
+    <Button label="Default" type="default" defaultFade />
+  ))
+  .add("Default Large", () => (
+    <Button label="Default Large" type="default" defaultLarge />
+  ))
+  .add("Default Large-Fade", () => (
+    <Button label="Default Large" type="default" defaultLargeFade />
+  ))
+  .add("Default Large-Border", () => (
+    <Button label="Default Large" type="default" defaultLargeBorder />
+  ));
