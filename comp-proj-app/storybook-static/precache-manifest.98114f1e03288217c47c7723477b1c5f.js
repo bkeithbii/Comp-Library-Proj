6cd1903a0f60bae6d6c574e7e95fc9a95d177a48
Brian Brown II <bkeithbii@gmail.com>
2020-02-21 16:11:58 -0500

    Added storybook static directory

---
+++ comp-proj-app/storybook-static/precache-manifest.98114f1e03288217c47c7723477b1c5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c159b3cd1fad4d4fca794653342be94",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.36d54604e2983adc3c0f.bundle.js"
+  },
+  {
+    "url": "runtime~main.36d54604e2983adc3c0f.bundle.js"
+  },
+  {
+    "revision": "f254898cf53934eb61e3",
+    "url": "static/css/main.3f07080b.chunk.css"
+  },
+  {
+    "url": "vendors~main.36d54604e2983adc3c0f.bundle.js"
+  },
+  {
+    "url": "vendors~main.36d54604e2983adc3c0f.bundle.js.LICENSE.txt"
+  }
+]);
